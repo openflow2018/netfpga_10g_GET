5b2da744d8d1e64f7797b703f4e9ef651c86365d
Viet-An <vietan.ctes@gmail.com>
2018-05-10 10:25:09 +0700

    create get pkts

--- contrib-projects/openflow_switch/hw/axi_simulation/flowtable_tests_scripts/get_filter/of_make_pkts.py
+++ contrib-projects/openflow_switch/hw/axi_simulation/flowtable_tests_scripts/get_filter/of_make_pkts.py
@@ -41,7 +41,7 @@ for i in range(0, 10):
     pkt = (Ether(src='f0:0d:f0:0d:f0:0d', dst='ba:be:ba:be:ba:be')/
            IP(src='204.204.204.204', dst='221.221.221.221')/
            TCP(sport=51996, dport=80)/
-           "GET /newyorker.com")
+           "GET /newyorker.com.abdcldkj")
     pkt.time = i*(1e-8)
     pkts.append(pkt)
              
